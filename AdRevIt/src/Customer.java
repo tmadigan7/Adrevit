a78809cbe715d57bfcd80d97d36a4680a156199b
tmadigan7 <tmadigan7@hotmail.com>
2014-05-21 21:27:53 -0500

    tmadigan7: starting customer

--- AdRevIt/src/Customer.java
+++ AdRevIt/src/Customer.java
@@ -1,5 +1,33 @@
-class Customer{
-
-public static void main(String [] args){
-}
+class Customer {
+	private String username;
+	private String first;
+	private String last;
+	private String email;
+	
+/***************** GETTERS AND SETTERS *****************/
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
